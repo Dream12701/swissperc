03b91b0e39f9e8d79483aedf1666568a1c0fc4bc
dream layerbobo <panyaporn14@gmail.com>
2024-08-11 11:50:37 +0000

    withdeploy.js

---
+++ scripts/mint.js
@@ -0,0 +1,34 @@
+const hre = require("hardhat");
+const { encryptDataField } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0xd0ce2bfD18206Afa171708D11303B959f84D6142";
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("PERC20Sample");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+  await mint100TokensTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${mint100TokensTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
